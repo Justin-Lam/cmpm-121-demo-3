4739695312b9eff6a3665ecf6a7c5afd9d468b6a
Justin-Lam <justinflam9225k@gmail.com>
2024-11-02 17:01:41 -0700

    Added a button to verify app works

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-// todo
+import "./style.css";
+const APP_NAME: string = "Geocoin GO";
+const app: HTMLDivElement = document.querySelector<HTMLDivElement>("#app")!;
+document.title = APP_NAME;
+
+const testButton = document.createElement("button");
+testButton.innerHTML = "Click me!";
+testButton.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+app.append(testButton);
