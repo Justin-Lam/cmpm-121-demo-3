6727cc0bad371f4babc2702f7fc7bd8a9d859db1
Justin-Lam <justinflam9225k@gmail.com>
2024-11-04 16:50:03 -0800

    Adjusted code to make more sense to me

--- src/main.ts
+++ src/main.ts
@@ -13,90 +13,108 @@ import luck from "./luck.ts";
 
 // App
 const APP_NAME: string = "Geocoin GO";
+
+// Locations
+const ORIGIN: leaflet.LatLng = leaflet.latLng( // aka Oakes classroom
+  36.98949379578401,
+  -122.06277128548504,
+);
+
+// Gameplay Parameters
+const ZOOM: number = 19;
+const TILE_DEGREES: number = 1e-4; // 0.0001
+const NEIGHBORHOOD_SIZE: number = 8;
+const CACHE_SPAWN_PROBABILITY: number = 0.1;
+const CACHE_MAX_INITIAL_COINS: number = 10; // pretty sure this is exclusive
+
+// Coins
+let playerCoins: number = 0;
+
+// App
 //const app: HTMLDivElement = document.querySelector<HTMLDivElement>("#app")!;
 document.title = APP_NAME;
 
-// Location of our classroom (as identified on Google Maps)
-const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
-
-// Tunable gameplay parameters
-const GAMEPLAY_ZOOM_LEVEL = 19;
-const TILE_DEGREES = 1e-4;
-const NEIGHBORHOOD_SIZE = 8;
-const CACHE_SPAWN_PROBABILITY = 0.1;
-
-// Create the map (element with id "map" is defined in index.html)
-const map = leaflet.map(document.getElementById("map")!, {
-  center: OAKES_CLASSROOM,
-  zoom: GAMEPLAY_ZOOM_LEVEL,
-  minZoom: GAMEPLAY_ZOOM_LEVEL,
-  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+// Create map
+const map: leaflet.Map = leaflet.map(document.getElementById("map")!, {
+  center: ORIGIN,
+  zoom: ZOOM,
+  minZoom: ZOOM,
+  maxZoom: ZOOM,
   zoomControl: false,
   scrollWheelZoom: false,
 });
 
-// Populate the map with a background tile layer
+// Give map tile layer
 leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: ZOOM,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-// Add a marker to represent the player
-const playerMarker = leaflet.marker(OAKES_CLASSROOM);
+// Create player marker
+const playerMarker: leaflet.Marker = leaflet.marker(ORIGIN);
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
 
-// Display the player's points
-let playerPoints = 0;
-const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!; // element `statusPanel` is defined in index.html
-statusPanel.innerHTML = "No points yet...";
-
-// Add caches to the map by cell numbers
-function spawnCache(i: number, j: number) {
-  // Convert cell numbers into lat/lng bounds
-  const origin = OAKES_CLASSROOM;
-  const bounds = leaflet.latLngBounds([
-    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
-    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
-  ]);
-
-  // Add a rectangle to the map to represent the cache
-  const rect = leaflet.rectangle(bounds);
-  rect.addTo(map);
-
-  // Handle interactions with the cache
-  rect.bindPopup(() => {
-    // Each cache has a random point value, mutable by the player
-    let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
-
-    // The popup offers a description and button
-    const popupDiv = document.createElement("div");
-    popupDiv.innerHTML = `
-                <div>There is a cache here at "${i},${j}". It has value <span id="value">${pointValue}</span>.</div>
-                <button id="poke">poke</button>`;
-
-    // Clicking the button decrements the cache's value and increments the player's points
-    popupDiv
-      .querySelector<HTMLButtonElement>("#poke")!
-      .addEventListener("click", () => {
-        pointValue--;
-        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-          pointValue.toString();
-        playerPoints++;
-        statusPanel.innerHTML = `${playerPoints} points accumulated`;
-      });
-
-    return popupDiv;
-  });
+// Show player inventory
+const inventoryPanel: HTMLDivElement = document.querySelector<HTMLDivElement>(
+  "#inventoryPanel",
+)!;
+updateInventoryPanelText();
+function updateInventoryPanelText() {
+  inventoryPanel.innerHTML = `${playerCoins} coins in inventory`;
 }
 
-// Look around the player's neighborhood for caches to spawn
+// Spawn neighborhood caches
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
   for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-    // If location i,j is lucky enough, spawn a cache!
     if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
       spawnCache(i, j);
     }
   }
+}
+/** Adds a cache to the map given its cell number. */
+function spawnCache(i: number, j: number) {
+  // Convert cell numbers to lat/lng bounds
+  const bounds: leaflet.LatLngBounds = leaflet.latLngBounds([
+    [
+      ORIGIN.lat + i * TILE_DEGREES,
+      ORIGIN.lng + j * TILE_DEGREES,
+    ],
+    [
+      ORIGIN.lat + (i + 1) * TILE_DEGREES,
+      ORIGIN.lng + (j + 1) * TILE_DEGREES,
+    ],
+  ]);
+
+  // Add cache (a rectangle) to map
+  const cache: leaflet.Rectangle = leaflet.rectangle(bounds);
+  cache.addTo(map);
+
+  // Add cache to map
+  cache.bindPopup(() => {
+    // Set coins
+    let coins: number = Math.floor(
+      luck([i, j, "initialValue"].toString()) * CACHE_MAX_INITIAL_COINS,
+    );
+
+    // Set popup description and button
+    const popup: HTMLDivElement = document.createElement("div");
+    popup.innerHTML = `
+			<div>This is cache (${i},${j}). It has <span id="coins">${coins}</span> coin(s).</div>
+			<button id="collectButton">Collect</button>
+		`;
+
+    // Set collect button on click event
+    popup.querySelector<HTMLButtonElement>("#collectButton")!
+      .addEventListener("click", () => {
+        coins--;
+        popup.querySelector<HTMLSpanElement>("#coins")!.innerHTML = coins
+          .toString();
+        playerCoins++;
+        updateInventoryPanelText();
+      });
+
+    return popup;
+  });
 }
